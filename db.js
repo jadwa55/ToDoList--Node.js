db8a353530fec90b40b5104fc62d19e88398ee1f
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2021-12-22 16:45:58 +0100

    connection sql

---
+++ db.js
@@ -0,0 +1,11 @@
+// const mysql = require('mysql');
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'user',
+//   password: '',
+//   database: 'databasen'
+// });
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log('Connected!');
+// });
--- models/tasksModel.js
+++ models/tasksModel.js
@@ -1,3 +1,21 @@
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'databasen',
+    multipleStatements: true
+
+});
+connection.connect((err) => {
+    if (err){
+        console.log(err)
+    };
+    console.log('Connected!');
+});
+
+
+
+
 let tasks = require('../data/tasks')
 const { v4: uuidv4 } = require('uuid')
 
@@ -8,6 +26,19 @@ function findAll() {
         resolve(tasks)
     })
 }
+// getAll = ()=>{
+//     return new Promise((resolve,reject)=>{
+//         let sql = "SELECT * FROM projects"
+//         connection.query(sql,(err,result)=>{
+//             if(err){
+//                 reject(err)
+//             }else{
+//                 resolve(result) 
+//             }
+//         })
+
+
+//     })
 
 function findById(id) {
     return new Promise((resolve, reject) => {
@@ -27,9 +58,9 @@ function create(task) {
 
 function update(id, task) {
     return new Promise((resolve, reject) => {
-        const index = task.findIndex((p) => p.id === id)
+        const index = tasks.findIndex((p) => p.id === id)
         tasks[index] = {id, ... task}
-        writeDataToFile('./data/tasks.json', task)
+        writeDataToFile('./data/tasks.json', tasks)
         resolve(tasks[index])
     })
 }
